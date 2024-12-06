2ae6553d221d7552d7872b9a8423d144f065fa08
ahmed-eishtawi <144367916+ahmed-eishtawi@users.noreply.github.com>
2024-12-07 00:41:51 +0200

    deploy

--- src/utils/ffmpegUtils.js
+++ src/utils/ffmpegUtils.js
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 
 export const encodeVideo = (inputFilePath, outputFilePath, codec, qp) => {
   return new Promise((resolve, reject) => {
-    const command = `ffmpeg -i ${inputFilePath} -c:v ${codec} -qp ${qp} -preset fast ${outputFilePath}`;
+    const command = `ffmpeg -i ${inputFilePath} -c:v ${codec} -qp ${qp} -preset fast "${outputFilePath}"`;
 
     exec(command, { maxBuffer: 1024 * 1024 * 10 }, (error, stdout, stderr) => {
       if (error) {
