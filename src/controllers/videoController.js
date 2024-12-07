265c81a8c0cd2f84617eb42057cb54444288189b
ahmed-eishtawi <144367916+ahmed-eishtawi@users.noreply.github.com>
2024-12-07 15:07:32 +0200

    general update

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -11,16 +11,12 @@ import {
 } from "../utils/metricsUtils.js";
 import upload from "../middlewares/multerConfig.js";
 
-export const uploadVideo = async (req, res) => {
-  /* the result array */
-  let results = [];
-
+export const uploadVideo = (req, res) => {
   upload.single("video")(req, res, async (error) => {
     if (error) {
       if (error.status === 400) {
         return res.status(400).json({ error: error.message });
       }
-      console.log(error)
       return res
         .status(500)
         .json({ error: "An internal server error occurred!" });
@@ -30,6 +26,15 @@ export const uploadVideo = async (req, res) => {
       return res
         .status(400)
         .json({ error: "No file uploaded or file type not supported." });
+    }
+
+    /* extract the QP value */
+    const qp = parseInt(req.body.qp, 10);
+
+    if (isNaN(qp) || qp < 0 || qp > 51) {
+      return res.status(400).json({
+        error: "Invalid QP value. Must be an integer between 0 and 51.",
+      });
     }
 
     const uploaded_file_name = req.file.filename;
@@ -45,72 +50,66 @@ export const uploadVideo = async (req, res) => {
     mkdirp.sync(h264_directory);
     mkdirp.sync(h265_directory);
 
+    const h264_output_path = path.relative(
+      process.cwd(),
+      path.join(h264_directory, `${file_name}_h264_qp_${qp}.mp4`)
+    );
+    const h265_output_path = path.relative(
+      process.cwd(),
+      path.join(h265_directory, `${file_name}_h265_qp_${qp}.mp4`)
+    );
+
     try {
-      for (const qp of QP_values) {
-        const h264_output_path = path.relative(
-          process.cwd(),
-          path.join(h264_directory, `${file_name}_h264_qp_${qp}.mp4`)
-        );
-        const h265_output_path = path.relative(
-          process.cwd(),
-          path.join(h265_directory, `${file_name}_h265_qp_${qp}.mp4`)
-        );
+      await encodeVideo(input_file_path, h264_output_path, "libx264", qp);
+      await encodeVideo(input_file_path, h265_output_path, "libx265", qp);
 
-        await encodeVideo(input_file_path, h264_output_path, "libx264", qp);
-        await encodeVideo(input_file_path, h265_output_path, "libx265", qp);
+      const h264_metrics = await extractMetrics(h264_output_path);
+      const h265_metrics = await extractMetrics(h265_output_path);
 
-        const h264_metrics = await extractMetrics(h264_output_path);
-        const h265_metrics = await extractMetrics(h265_output_path);
+      const h264_bitrate = getVideoBitrate(h264_metrics);
+      const h265_bitrate = getVideoBitrate(h265_metrics);
 
-        const h264_bitrate = getVideoBitrate(h264_metrics);
-        const h265_bitrate = getVideoBitrate(h265_metrics);
+      const psnr_h264 = await calculatePSNR(h264_output_path, input_file_path);
+      const psnr_h265 = await calculatePSNR(h265_output_path, input_file_path);
 
-        const psnr_h264 = await calculatePSNR(
-          h264_output_path,
-          input_file_path
-        );
-        const psnr_h265 = await calculatePSNR(
-          h265_output_path,
-          input_file_path
-        );
-
-        // Append result for this QP value
-        results.push({
-          h264: {
-            bitrate: h264_bitrate,
-            qp: qp,
-            psnr: psnr_h264,
-          },
-          h265: {
-            bitrate: h265_bitrate,
-            qp: qp,
-            psnr: psnr_h265,
-          },
-          differences: {
-            bitrate: Number(Math.abs(h264_bitrate - h265_bitrate).toFixed(7)),
-            qp: qp,
-            psnr: Number(Math.abs(psnr_h264 - psnr_h265).toFixed(7)),
-          },
-        });
-      }
+      // Append result for this QP value
+      const result = {
+        h264: {
+          bitrate: h264_bitrate,
+          qp: qp,
+          psnr: psnr_h264,
+        },
+        h265: {
+          bitrate: h265_bitrate,
+          qp: qp,
+          psnr: psnr_h265,
+        },
+        differences: {
+          bitrate: Number(Math.abs(h264_bitrate - h265_bitrate).toFixed(7)),
+          qp: qp,
+          psnr: Number(Math.abs(psnr_h264 - psnr_h265).toFixed(7)),
+        },
+      };
 
       /* Send success response */
       res.status(200).json({
         message: `Your video (${req.file.originalname}) uploaded and processed successfully!`,
-        results,
+        result,
       });
     } catch (error) {
-      /* Send error response */
+      /* Send error response if any error occurs*/
       res.status(500).json({ error: error.message });
     } finally {
       /* Delete the uploaded file */
       fs.unlinkSync(input_file_path);
 
       /* Delete the encoded videos for each (h264, h265) */
-      for (const qp of QP_values) {
-        fs.unlinkSync(path.join(h264_directory, `${file_name}_h264_qp_${qp}.mp4`));
-        fs.unlinkSync(path.join(h265_directory, `${file_name}_h265_qp_${qp}.mp4`));
-      }
+      fs.unlinkSync(
+        path.join(h264_directory, `${file_name}_h264_qp_${qp}.mp4`)
+      );
+      fs.unlinkSync(
+        path.join(h265_directory, `${file_name}_h265_qp_${qp}.mp4`)
+      );
     }
   });
 };
